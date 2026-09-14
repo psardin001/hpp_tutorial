e731860213e792b17906b953cffdff3994237fb8
Paul Sardin <paulsardin123@gmail.com>
2026-09-14 11:07:53 +0200

    [exercise_10] Apply the configured Python style

--- exercise_10/pick_and_place.py
+++ exercise_10/pick_and_place.py
@@ -1,13 +1,16 @@
 import numpy as np
 from pinocchio import SE3, neutral
-from pyhpp.constraints import (ComparisonType, ComparisonTypes, Implicit, Position)
-from pyhpp.core import (ConfigProjector, Discretized, Progressive, RandomShortcut)
-from pyhpp.manipulation import (Device, Graph, GraphPathValidation,
-                                Problem, ManipulationPlanner, SplineGradientBased_bezier3, urdf)
+from pyhpp.core import ConfigProjector, Progressive, RandomShortcut
+from pyhpp.manipulation import (
+    Device,
+    Graph,
+    GraphPathValidation,
+    ManipulationPlanner,
+    Problem,
+    urdf,
+)
 from pyhpp.manipulation.constraint_graph_factory import ConstraintGraphFactory
 from pyhpp_toppra import Toppra
-from pyhpp_viser import Viewer
-
 
 robot = Device("mfja")
 
@@ -15,30 +18,41 @@ robot = Device("mfja")
 urdf_filename = "package://mfja_3rd_floor_description/urdf/staubli_tx2_60l.urdf"
 srdf_filename = "package://mfja_3rd_floor_description/srdf/staubli_tx2_60l.srdf"
 
-urdf.loadModel(robot, 0, "staubli", "anchor", urdf_filename, srdf_filename, SE3.Identity())
+urdf.loadModel(
+    robot, 0, "staubli", "anchor", urdf_filename, srdf_filename, SE3.Identity()
+)
 
 # Load gear plate
 urdf_filename = "package://mfja_3rd_floor_description/urdf/gear_plate.urdf"
 srdf_filename = "package://mfja_3rd_floor_description/srdf/gear_plate.srdf"
 pose = SE3.Identity()
-pose.translation = np.array([.6, .15, 0.])
+pose.translation = np.array([0.6, 0.15, 0.0])
 
-urdf.loadModel(robot, 0, "gear_plate", "anchor", urdf_filename, srdf_filename,
-               pose)
+urdf.loadModel(robot, 0, "gear_plate", "anchor", urdf_filename, srdf_filename, pose)
 
 # Load 42 mm gear
 urdf_filename = "package://mfja_3rd_floor_description/urdf/gear_42.urdf"
 srdf_filename = "package://mfja_3rd_floor_description/srdf/gear_42.srdf"
 
-urdf.loadModel(robot, 0, "gear_42", "freeflyer", urdf_filename, srdf_filename, SE3.Identity())
+urdf.loadModel(
+    robot, 0, "gear_42", "freeflyer", urdf_filename, srdf_filename, SE3.Identity()
+)
 
-robot.setJointBounds("gear_42/root_joint", [-1., 1.,
-    -1., 1.,
-    -0.2, 1.5,])
+robot.setJointBounds(
+    "gear_42/root_joint",
+    [
+        -1.0,
+        1.0,
+        -1.0,
+        1.0,
+        -0.2,
+        1.5,
+    ],
+)
 
 problem = Problem(robot)
-problem.pathValidation(GraphPathValidation(Progressive(robot, .001)))
-problem.pathValidationFactory(GraphPathValidation(Progressive(robot, .001)))
+problem.pathValidation(GraphPathValidation(Progressive(robot, 0.001)))
+problem.pathValidationFactory(GraphPathValidation(Progressive(robot, 0.001)))
 
 graph = Graph("robot", robot, problem)
 factory = ConstraintGraphFactory(graph)
@@ -55,17 +69,18 @@ factory.generate()
 
 # Deactive collision checking between gripper and gear_42 when grasped
 for tr in [
-        "staubli/tool0_gripper > gear_42/stud | f_12",
-        "staubli/tool0_gripper < gear_42/stud | 0-0_21",
-        "staubli/tool0_gripper > gear_42/stud | f_23",
-        "staubli/tool0_gripper < gear_42/stud | 0-0_32",
-        "staubli/tool0_gripper > gear_42/stud | f_34",
-        "staubli/tool0_gripper < gear_42/stud | 0-0_43",
-        "Loop | 0-0",
+    "staubli/tool0_gripper > gear_42/stud | f_12",
+    "staubli/tool0_gripper < gear_42/stud | 0-0_21",
+    "staubli/tool0_gripper > gear_42/stud | f_23",
+    "staubli/tool0_gripper < gear_42/stud | 0-0_32",
+    "staubli/tool0_gripper > gear_42/stud | f_34",
+    "staubli/tool0_gripper < gear_42/stud | 0-0_43",
+    "Loop | 0-0",
 ]:
     transition = graph.getTransition(tr)
-    graph.setSecurityMarginForTransition(transition, "staubli/joint_6", "gear_42/root_joint",
-                                         float("-inf"))
+    graph.setSecurityMarginForTransition(
+        transition, "staubli/joint_6", "gear_42/root_joint", float("-inf")
+    )
 
 graph.initialize()
 
@@ -103,6 +118,6 @@ p1 = opt1.optimize(p)
 toppra = Toppra(problem)
 toppra.velocityScale = 0.5
 toppra.N = 100
-toppra.selectJoints([f"staubli/joint_{i}" for i in range(1,7)])
+toppra.selectJoints([f"staubli/joint_{i}" for i in range(1, 7)])
 toppra.accelerationLimits = np.array(6 * [0.5])
 p2 = toppra.optimize(p1)
